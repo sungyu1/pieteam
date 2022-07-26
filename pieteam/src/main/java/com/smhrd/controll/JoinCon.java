7573861388814dc9876abf31229039f5a78b2001
smhrd <smhrd@220.80.203.55>
2022-07-26 16:03:23 +0900

    0726 첫개시

---
+++ pieteam/src/main/java/com/smhrd/controll/JoinCon.java
@@ -0,0 +1,29 @@
+package com.smhrd.controll;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class JoinCon
+ */
+public class JoinCon extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public JoinCon() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
